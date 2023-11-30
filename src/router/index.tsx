54e6918f00da52fb4c25a4185084a92e7fa9d223
Ersola <hqy1012927920@gmail.com>
2023-11-30 19:37:06 +0800

    #CHORE: test about

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,23 @@
-import { RouteConfig } from 'react-router-config';
-import HomePage from "../pages/homePage";
+import { RouteConfig } from 'react-router-config'
+import HomePage from '../pages/homePage'
+import AboutView from '@/pages/About'
 // 导入页面组件
 
 const routes: RouteConfig[] = [
-    {
-        path: '/',
-        exact: true,
-        component: HomePage,
-        // 可选的其他配置
-        meta: {
-            title: 'Home',
-            description: 'Welcome to the home page',
-        },
+  {
+    path: '/',
+    exact: true,
+    component: HomePage,
+    // 可选的其他配置
+    meta: {
+      title: 'Home',
+      description: 'Welcome to the home page',
     },
-];
+  },
+  {
+    path: 'about',
+    component: AboutView,
+  }
+]
 
-export default routes;
+export default routes
